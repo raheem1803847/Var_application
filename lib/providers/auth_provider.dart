65cd645d6f45426a818e20a8378e453172208cf6
raheem1803847 <raheem1803847@miuegypt.edu.eg>
2021-12-13 18:12:18 +0200

    provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+enum Status {
+  NotLoggedIn,
+  NotRegistered,
+  LoggedIn,
+  Registered,
+  Authenticating,
+  Registering,
+  LoggedOut
+}
+
+class AuthProvider extends ChangeNotifier {
+  Status _loggedInStatus = Status.NotLoggedIn;
+  Status _registeredInStatus = Status.NotRegistered;
+
+  Status get loggedInStatus => _loggedInStatus;
+
+  set loggedInStatus(Status value) {
+    _loggedInStatus = value;
+  }
+
+  Status get registeredInStatus => _registeredInStatus;
+
+  set registeredInStatus(Status value) {
+    _registeredInStatus = value;
+  }
+}
