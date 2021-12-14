4917809d72d7e8ee68b1df131ec20aa096927306
heeego26 <96143636+heeego26@users.noreply.github.com>
2021-12-15 00:18:14 +0200

    tablescreen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utilites/LeagueContainer.dart';
 import 'package:flutter_application_1/utilites/menuBar.dart';
-
+import 'package:flutter_application_1/screens/TableScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -16,22 +16,22 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
-             image: DecorationImage(
-                  image: AssetImage('assets/images/mosalah.jpg'),
-                  fit: BoxFit.cover,
-                  colorFilter:
-                      ColorFilter.mode(Colors.white10, BlendMode.colorDodge),
-                ),
+              image: DecorationImage(
+                image: AssetImage('assets/images/mosalah.jpg'),
+                fit: BoxFit.cover,
+                colorFilter:
+                    ColorFilter.mode(Colors.white10, BlendMode.colorDodge),
+              ),
               gradient: LinearGradient(
-            colors: [
-              const Color(0xffe84860),
-              const Color(0xffe70066),
-            ],
-            begin: const FractionalOffset(0.0, 0.0),
-            end: const FractionalOffset(0.0, 1.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp,
-          )),
+                colors: [
+                  const Color(0xffe84860),
+                  const Color(0xffe70066),
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(0.0, 1.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              )),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: ListView(
@@ -57,23 +57,65 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisSpacing: 15,
                   children: [
                     GestureDetector(
-                      child: LeagueContainer(image:('assets/images/pl.png')),
+                      child: LeagueContainer(image: ('assets/images/pl.png')),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PL'),
+                            ));
+                      },
                     ),
                     GestureDetector(
                       child: LeagueContainer(image: 'assets/images/laliga.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PD'),
+                            ));
+                      },
                     ),
                     GestureDetector(
-                      child: LeagueContainer(image: 'assets/images/bundesliga.png'),
+                      child: LeagueContainer(
+                          image: 'assets/images/bundesliga.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'BL1'),
+                            ));
+                      },
                     ),
                     GestureDetector(
                       child: LeagueContainer(image: 'assets/images/seria.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'SA'),
+                            ));
+                      },
                     ),
                     GestureDetector(
                       child: LeagueContainer(image: 'assets/images/ligue1.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'FL1'),
+                            ));
+                      },
                     ),
                     GestureDetector(
                       child: LeagueContainer(image: 'assets/images/nos.png'),
-                      
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PPL'),
+                            ));
+                      },
                     ),
                   ],
                 )
