7db25766c1cd627f887b0222ab2fd4e01eba0c36
heeego26 <83840210+beeego26@users.noreply.github.com>
2021-12-15 00:38:06 +0200

    1

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Profile",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Body(),
+    );
+  }
+}
