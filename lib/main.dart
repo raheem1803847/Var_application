2603d90232f07ef52a85d81bf2dc8890dfdb78e7
raheem1803847 <raheem1803847@miuegypt.edu.eg>
2022-01-22 03:01:16 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Body(),
+        home: SplashScreen(),
         routes: {
           '/login': (context) => login(),
           '/register': (context) => register(),
